37f569f73d655c74061f166fd591dffcc73234ac
Yvette Jing <mutongjing@gmail.com>
2026-09-22 13:16:57 -0700

    Add files via upload

---
+++ date.java
@@ -0,0 +1,15 @@
+public class date
+
+	public static void printAmerican(String day, String month, int date, int year) {
+		System.out.print(day + ", " + month + " " + date + ", " + year) {
+	}
+
+	public static void printEuropean(String
+	public static void main (String[] args) {
+	
+	printAmerican("Tuesday", "September", 22, 2026);
+	
+	}
+}
+
+//System.out.print("European format: " + day + " " + date + " " + month + " " + year);
