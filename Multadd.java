bf71cd1b847b99450b466c54882e64a152c6c56f
Yvette Jing <mutongjing@gmail.com>
2026-09-24 13:25:12 -0700

    Add files via upload

--- Multadd.java
+++ Multadd.java
@@ -5,7 +5,7 @@ public static double multadd(double a, double b, double c) {
 }
 
 public static double expSum(double x) {
-	return multadd(1.0, multadd(x, Math.exp(-x), 0), (Math.sqrt(1 - Math.exp(-x))));
+	return multadd(1.0, x * Math.exp(-x), Math.sqrt(1 - Math.exp(-x)));
 }
 
 public static void main (String[] args) {
